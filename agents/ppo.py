223de3a9371cee560821891beaf538484f519990
amin-tabrizian <amin_tabrizian@live.com>
2024-07-18 11:21:33 -0400

    perf: update lk and lc agent network architecture

--- agents/ppo.py
+++ agents/ppo.py
@@ -48,16 +48,23 @@ class ActorCritic(nn.Module):
         # actor
         if has_continuous_action_space :
             self.actor = nn.Sequential(
-                            nn.Linear(state_dim, 128),
+                            nn.Linear(state_dim, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 128),
                             nn.Tanh(),
                             nn.Linear(128, 64),
                             nn.Tanh(),
-                            nn.Linear(64, action_dim),
-                            nn.Tanh()
+                            nn.Linear(64, action_dim)
                         )
         else:
             self.actor = nn.Sequential(
-                            nn.Linear(state_dim, 128),
+                            nn.Linear(state_dim, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 128),
                             nn.Tanh(),
                             nn.Linear(128, 64),
                             nn.Tanh(),
@@ -66,11 +73,15 @@ class ActorCritic(nn.Module):
                         )
         # critic
         self.critic = nn.Sequential(
-                        nn.Linear(state_dim, 128),
-                        nn.Tanh(),
-                        nn.Linear(128, 64),
-                        nn.Tanh(),
-                        nn.Linear(64, 1)
+                        nn.Linear(state_dim, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 256),
+                            nn.Tanh(),
+                            nn.Linear(256, 128),
+                            nn.Tanh(),
+                            nn.Linear(128, 64),
+                            nn.Tanh(),
+                            nn.Linear(64, 1)
                     )
         
     def set_action_std(self, new_action_std):
